9f7f31fa15c9c7cb4bf801d57aeaa11f1f8a7aa2
Gaurav Paidukle <gauravpaidukle@Gauravs-iMac.local>
2023-05-15 14:42:32 +0530

    Update Flask application code

--- app.py
+++ app.py
@@ -1,39 +1,44 @@
+from flask import Flask, render_template, request
 from youtube_transcript_api import YouTubeTranscriptApi
 import openai
 
+app = Flask(__name__)
+
 # Set up OpenAI API credentials
-openai.api_key = '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'
+openai.api_key = 'API_KEY HERE'
 
-# Specify the video ID or URL
-video_id = "8musxkEil6w"
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        # Get the YouTube video ID from the form
+        video_id = request.form['video_id']
+        
+        # Retrieve the transcript for the video
+        transcript = YouTubeTranscriptApi.get_transcript(video_id)
+        
+        # Extract the text from the transcript
+        text = ""
+        for line in transcript:
+            text += line['text'] + " "
+        
+        # Generate the optimized blog post using OpenAI's API
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=text,
+            temperature=0.7,
+            max_tokens=500,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.6,
+            stop=None,
+        )
+        
+        # Extract the generated blog post
+        blog_post = response.choices[0].text.strip()
+        
+        return render_template('index.html', blog_post=blog_post)
+    
+    return render_template('index.html', blog_post='')
 
-# Retrieve the transcript for the video
-transcript = YouTubeTranscriptApi.get_transcript(video_id)
-
-# Extract the text from the transcript
-text = ""
-for line in transcript:
-    text += line['text'] + " "
-
-# Generate the optimized blog post using OpenAI's API
-response = openai.Completion.create(
-    engine="text-davinci-003",
-    prompt="Make this in the form of an Seo Blog Post" + text,
-    temperature=0.7,
-    max_tokens=500,
-    top_p=1.0,
-    frequency_penalty=0.0,
-    presence_penalty=0.6,
-    stop=None,
-)
-
-# Extract the generated blog post
-blog_post = response.choices[0].text.strip()
-
-# Save the blog post to a file
-output_file = "blog_post.txt"
-with open(output_file, "w") as file:
-    file.write(blog_post)
-
-# Print the success message
-print("Blog post generated and saved to", output_file)
+if __name__ == '__main__':
+    app.run(debug=True)
